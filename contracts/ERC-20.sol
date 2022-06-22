7577728a288f60a054f7e095216631454eaa4309
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-06-22 15:24:59 +0300

    added contract variables and view funcitons

--- contracts/ERC-20.sol
+++ contracts/ERC-20.sol
@@ -4,8 +4,34 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract ERC_20 {
+    mapping (address => uint256) _balances;
 
-    constructor() {
+    string private _name;
+    string private _symbol;
+    uint256 private _totalSupply;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
     }
 
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure virtual returns(uint8) {
+        return 18;
+    }
+
+    function totalSupply() public view virtual returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return _balances[_owner];
+    }
 }
