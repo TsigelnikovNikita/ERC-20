a0b188a5f761fad5e5b662a6e7d4dea94207eee0
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-06-24 11:23:59 +0300

    changed name and symbol

--- arguments.js
+++ arguments.js
@@ -1,4 +1,4 @@
 module.exports = [
-    "Name",
-    "Symbol"
+    "CryptonStudio",
+    "CRS"
   ];
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,14 @@
 const hre = require("hardhat");
 
 const CONTRACT_NAME="ERC20";
-const TOKEN_NAME="name";
-const SYMBOL_NAME="symbol";
+const TOKEN_NAME="CryptonStudio";
+const SYMBOL_NAME="CRS";
 
 async function main() {
     const contractFactory = await hre.ethers.getContractFactory(CONTRACT_NAME);
 
     const contract = await contractFactory.deploy(TOKEN_NAME, SYMBOL_NAME);
+    console.log(`Transaction hash: ${contract.deployTransaction.hash}`);
     await contract.deployed();
     console.log(`${CONTRACT_NAME} deployed to: ${contract.address}`);
 }
